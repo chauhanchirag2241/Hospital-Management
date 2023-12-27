46ffe2df86632232d8747394de72bb7d80ac9cf9
chirag <coderchirag2272@gmail.com>
2023-12-27 23:29:23 +0530

    add script for serverroletable and severuser table and add data dictionary file

---
+++ Hospital Management System/DataBase/DataBaseScripts/M2_CreateServerRoleTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace DataBase.DataBaseScripts
+{
+    public class M2_CreateServerRoleTable : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("serverrole")
+                .WithColumn("serverroleid").AsInt64().PrimaryKey().Identity()
+                .WithColumn("serverrolename").AsString().NotNullable()
+                .WithColumn("description").AsString().Nullable()
+                .WithColumn("isactive").AsBoolean().NotNullable()
+                .WithColumn("createdby").AsInt64().NotNullable()
+                .WithColumn("createdon").AsDateTime().NotNullable()
+                .WithColumn("updatedby").AsInt64().NotNullable()
+                .WithColumn("updatedon").AsDateTime().NotNullable();
+
+        }
+    }
+}
---
+++ Hospital Management System/DataBase/DataBaseScripts/M3_CreateServerUserTable.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace DataBase.DataBaseScripts
+{
+    public class M3_CreateServerUserTable : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("serveruser")
+                 .WithColumn("serveruserid").AsInt64().PrimaryKey().Identity()
+                 .WithColumn("serverroleid").AsInt64().NotNullable()
+                 .WithColumn("username").AsString().NotNullable()
+                 .WithColumn("password").AsString().NotNullable()
+                 .WithColumn("mobileno").AsCustom("varchar(15)").Nullable()
+                 .WithColumn("email").AsString().Nullable()
+                 .WithColumn("isactive").AsBoolean().NotNullable()
+                 .WithColumn("createdby").AsInt64().NotNullable()
+                 .WithColumn("createdon").AsDateTime().NotNullable()
+                 .WithColumn("updatedby").AsInt64().NotNullable()
+                 .WithColumn("updatedon").AsDateTime().NotNullable();
+
+
+        }
+    }
+}
