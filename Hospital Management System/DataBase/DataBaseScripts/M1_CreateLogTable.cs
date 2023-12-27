b968b8173b5f1af3670b6789399e3e9cbdabfc22
chirag.chauhan <chirag.chauhan@promptsoftech.in>
2023-12-27 14:54:02 +0530

    add git ignore to all projects.

---
+++ Hospital Management System/DataBase/DataBaseScripts/M1_CreateLogTable.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace DataBase
+{
+    [Migration(1)]
+    public class AddLogTable : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Log")
+             .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+             .WithColumn("Text").AsString();
+        }
+    }
+}
+    
--- Hospital Management System/DataBase/Program.cs
+++ Hospital Management System/DataBase/Program.cs
@@ -1,7 +1,54 @@
-﻿    public class Program
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataBase
+{
+    public class Program
     {
-        public static void Main()
+        static void Main()
         {
-            Console.WriteLine("hello world");
+            using (var serviceProvider = CreateServices())
+            using (var scope = serviceProvider.CreateScope())
+            {
+                // Put the database update into a scope to ensure
+                // that all resources will be disposed.
+                UpdateDatabase(scope.ServiceProvider);
+            }
+
         }
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static ServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Server=DT095\\SQLEXPRESS;Database= hms_config;Trusted_Connection=True;TrustServerCertificate=True;")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(AddLogTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+
+
     }
+}
